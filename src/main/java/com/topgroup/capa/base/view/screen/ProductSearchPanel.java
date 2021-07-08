94d63846b4f8640d5748be9e1a4a2c4ca9ce14a9
sanntibenitez <sanntibenitez@gmail.com>
2021-07-08 16:09:20 -0300

    First commit

---
+++ src/main/java/com/topgroup/capa/base/view/screen/ProductSearchPanel.java
@@ -0,0 +1,61 @@
+package com.topgroup.capa.base.view.screen;
+
+import com.topgroup.commons.vaadin.view.bean.ViewBean;
+import com.topgroup.commons.vaadin.view.panel.BaseSearchPanel;
+import com.vaadin.data.Container;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.ui.Button.ClickEvent;
+
+public class ProductSearchPanel extends BaseSearchPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void valueChange(ValueChangeEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected ViewBean getBeanForm() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getColumnHeaders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Container getContainerDataSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object[] getVisibleColumns() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getVisibleItemProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void newEntity(ClickEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void search(ClickEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
